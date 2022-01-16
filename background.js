32d2c9ed3444421c50039e1214a05ec8c11f5ed3
Frank Hui <huifrank@live.com>
2022-01-15 23:19:09 -0800

    Add poggersalert

--- background.js
+++ background.js
@@ -1,5 +1,27 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
 	chrome.tabs.executeScript(tab.ib, {
 		file: 'inject.js'
 	});
+});
+
+chrome.action.onClicked.addListener((tab) => {
+	chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		func: contentScriptFunc,
+		args: ['action'],
+	});
+});
+
+function contentScriptFunc(name) {
+	Swal.fire({
+		title: 'Error!',
+		text: 'Do you want to continue',
+		icon: 'error',
+		confirmButtonText: 'Cool'
+	})
+}
+
+// This callback WILL NOT be called for "_execute_action"
+chrome.commands.onCommand.addListener((command) => {
+	console.log(`Command "${command}" called`);
 });
--- inject.js
+++ inject.js
@@ -1,44 +1,44 @@
 (function() {
 
-	const progressWrapper = document.createElement('div');
-	progressWrapper.className = 'progress-wrapper';
-  
-  const progressLabel = document.createElement('div');
-  progressLabel.className = 'progress-label';
-  const progressElement = document.createElement('progress');
+    const progressWrapper = document.createElement('div');
+    progressWrapper.className = 'progress-wrapper';
 
-  progressWrapper.style.position = 'fixed';
-	progressWrapper.style.top = 0;
-	// progressWrapper.style.right = 0;
-	// // progressWrapper.textContent = 'Injected!';
-  progressWrapper.style.zIndex = 10000;
+    const progressLabel = document.createElement('div');
+    progressLabel.className = 'progress-label';
+    const progressElement = document.createElement('progress');
 
-  progressWrapper.appendChild(progressLabel);
-  progressWrapper.appendChild(progressElement);
+    progressWrapper.style.position = 'fixed';
+    progressWrapper.style.top = 0;
+    // progressWrapper.style.right = 0;
+    // // progressWrapper.textContent = 'Injected!';
+    progressWrapper.style.zIndex = 10000;
 
-	document.getElementsByTagName('html')[0].prepend(progressWrapper);
+    progressWrapper.appendChild(progressLabel);
+    progressWrapper.appendChild(progressElement);
 
-  $(document).ready(function() {
-    const win = $(window);
-    const doc = $(document);
-    const progressBar = $('progress');
-    const progressLabel = $('.progress-label');
-    const setValue = () => win.scrollTop();
-    const setMax = () => doc.height() - win.height();
-    const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
-    
-    progressLabel.text(setPercent() + '%');
-    progressBar.attr({ value: setValue(), max: setMax() });
-  
-    doc.on('scroll', () => {
-      progressLabel.text(setPercent() + '%');
-      progressBar.attr({ value: setValue() });
+    document.getElementsByTagName('html')[0].prepend(progressWrapper);
+
+    $(document).ready(function() {
+        const win = $(window);
+        const doc = $(document);
+        const progressBar = $('progress');
+        const progressLabel = $('.progress-label');
+        const setValue = () => win.scrollTop();
+        const setMax = () => doc.height() - win.height();
+        const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
+
+        progressLabel.text(setPercent() + '%');
+        progressBar.attr({ value: setValue(), max: setMax() });
+
+        doc.on('scroll', () => {
+            progressLabel.text(setPercent() + '%');
+            progressBar.attr({ value: setValue() });
+        });
+
+        win.on('resize', () => {
+            progressLabel.text(setPercent() + '%');
+            progressBar.attr({ value: setValue(), max: setMax() });
+        })
     });
-    
-    win.on('resize', () => {
-      progressLabel.text(setPercent() + '%');
-      progressBar.attr({ value: setValue(), max: setMax() });
-    })
-  });
 
 })();
