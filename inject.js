5614961cadb5d4195ecaa1daa3b3db3c7e47d72f
Kai Chen <22359229+kx-chen@users.noreply.github.com>
2022-01-15 23:09:04 -0800

    Inject progress bar into every page

--- inject.js
+++ inject.js
@@ -1,12 +1,44 @@
 (function() {
 
-	const div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-  div.style.zIndex = 10000;
-	document.body.appendChild(div);
+	const progressWrapper = document.createElement('div');
+	progressWrapper.className = 'progress-wrapper';
+  
+  const progressLabel = document.createElement('div');
+  progressLabel.className = 'progress-label';
+  const progressElement = document.createElement('progress');
 
-	alert('rao!!!');
+  progressWrapper.style.position = 'fixed';
+	progressWrapper.style.top = 0;
+	// progressWrapper.style.right = 0;
+	// // progressWrapper.textContent = 'Injected!';
+  progressWrapper.style.zIndex = 10000;
+
+  progressWrapper.appendChild(progressLabel);
+  progressWrapper.appendChild(progressElement);
+
+	document.getElementsByTagName('html')[0].prepend(progressWrapper);
+
+  $(document).ready(function() {
+    const win = $(window);
+    const doc = $(document);
+    const progressBar = $('progress');
+    const progressLabel = $('.progress-label');
+    const setValue = () => win.scrollTop();
+    const setMax = () => doc.height() - win.height();
+    const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
+    
+    progressLabel.text(setPercent() + '%');
+    progressBar.attr({ value: setValue(), max: setMax() });
+  
+    doc.on('scroll', () => {
+      progressLabel.text(setPercent() + '%');
+      progressBar.attr({ value: setValue() });
+    });
+    
+    win.on('resize', () => {
+      progressLabel.text(setPercent() + '%');
+      progressBar.attr({ value: setValue(), max: setMax() });
+    })
+  });
+
 })();
