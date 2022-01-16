67f049fef691b7a36eb6f551b86b932bc23be995
Frank Hui <huifrank@live.com>
2022-01-16 01:34:43 -0800

    Add logos

--- background.js
+++ background.js
@@ -13,46 +13,40 @@ chrome.action.onClicked.addListener((tab) => {
 });
 
 function contentScriptFunc(name) {
-	// Swal.fire({
-	// 	position: 'top-end',
-	// 	icon: 'success',
-	// 	title: 'Page added!',
-	// 	showConfirmButton: false,
-	// 	timer: 5000,
-	// 	backdrop: false,
-	// 	toast: true,
-	// 	customClass: {
-	// 		border: '5px solid black'
-	// 	}
-	// })
+	Swal.fire({
+		position: 'top-end',
+		icon: 'success',
+		title: 'Page added!',
+		showConfirmButton: false,
+		timer: 5000,
+		backdrop: false,
+		toast: true,
+		customClass: {
+			border: '5px solid black'
+		}
+	})
+}
+
+function addTask(url, time){
+
+}
+
+function taskDone(name) {
 	Swal.fire({
 		title: 'Way to go!\n Ready to move on to the next task?',
 		showDenyButton: true,
 		confirmButtonText: 'Yes!',
 		denyButtonText: `More time.`,
 		denyButtonColor: '#c9a92d',
-		iconHtml: '<img src="https://raw.githubusercontent.com/MyModus/Modus/master/logo.svg">'
+		confirmButtonColor: 'DarkSeaGreen',
+		iconHtml: '<img src="https://raw.githubusercontent.com/MyModus/Modus/master/logo.svg" style="width: 6rem">',
+		width:'50%'
 	}).then((result) => {
 		/* Read more about isConfirmed, isDenied below */
 		if (result.isDenied) {
 			Swal.fire('5 minutes added.', '', 'success')
-		}
-	})
-}
-
-function taskDone(name) {
-	Swal.fire({
-		title: 'Do you want to save the changes?',
-		showDenyButton: true,
-		confirmButtonText: 'Next',
-		denyButtonText: `Don't save`,
-		denyButtonColor: '#ffdd55',
-	}).then((result) => {
-		/* Read more about isConfirmed, isDenied below */
-		if (result.isConfirmed) {
-			Swal.fire('Saved!', '', 'success')
-		} else if (result.isDenied) {
-			Swal.fire('Changes are not saved', '', 'info')
+		}else{
+			// Redirect here.
 		}
 	})
 }
